b27a068dacf8bd306b0530bacba75526425c275e
agriffi10 <agriffi10@gmail.com>
2022-03-09 20:18:24 -0500

    updating config and excluding tests from build

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const simple_deep_copy_1 = __importDefault(require("./simple-deep-copy/simple-deep-copy"));
+const generate_random_hex_color_1 = __importDefault(require("./generate-random-hex-color/generate-random-hex-color"));
+const debounce_1 = __importDefault(require("./debounce/debounce"));
 const cu_ = {
     simpleDeepCopy: simple_deep_copy_1.default,
+    generateRandomHexColor: generate_random_hex_color_1.default,
+    debounce: debounce_1.default,
 };
 for (const prop in cu_) {
     if (cu_.hasOwnProperty(prop)) {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import simpleDeepCopy from "./simple-deep-copy/simple-deep-copy";
+import generateRandomHexColor from "./generate-random-hex-color/generate-random-hex-color";
+import debounce from "./debounce/debounce";
 
 const cu_: {
   [key: string]: Function;
 } = {
   simpleDeepCopy,
+  generateRandomHexColor,
+  debounce,
 };
 
 for (const prop in cu_) {
